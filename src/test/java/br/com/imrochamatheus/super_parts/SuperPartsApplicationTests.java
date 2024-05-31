b7b2a893438c995da67a79a13fc1e7589ea726bd
imrochamatheus <im.rochamatheus@gmail.com>
2024-05-31 17:57:50 -0300

    :tada: initial commit

---
+++ src/test/java/br/com/imrochamatheus/super_parts/SuperPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imrochamatheus.super_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
