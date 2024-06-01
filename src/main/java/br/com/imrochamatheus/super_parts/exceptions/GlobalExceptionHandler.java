16d237adae83d3f5ab1a3fffd4d49061835fc256
imrochamatheus <im.rochamatheus@gmail.com>
2024-06-01 14:28:40 -0300

    :sparkles: feat: add global exception handler

---
+++ src/main/java/br/com/imrochamatheus/super_parts/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.com.imrochamatheus.super_parts.exceptions;
+
+import br.com.imrochamatheus.super_parts.utils.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        ErrorResponse errorResponse = new ErrorResponse(status, message);
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @ExceptionHandler({CarAlreadyExistsException.class})
+    public ResponseEntity<ErrorResponse> handleCarAlreadyExistsException (CarAlreadyExistsException exception) {
+        return this.buildErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleMethodNotValidException (MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getAllErrors().forEach(error -> {
+            String errorField = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(errorField, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+}
