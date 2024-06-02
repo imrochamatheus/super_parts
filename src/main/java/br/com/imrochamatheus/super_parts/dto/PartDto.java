d0f6e481b6235e61c80f53bafac3f2b978a53183
imrochamatheus <im.rochamatheus@gmail.com>
2024-06-02 12:20:06 -0300

    :sparkles: feat: add validation groups to parts dto

--- src/main/java/br/com/imrochamatheus/super_parts/dto/PartDto.java
+++ src/main/java/br/com/imrochamatheus/super_parts/dto/PartDto.java
@@ -13,40 +13,38 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PartDto {
-    @NotNull(message = "is is required",
-                groups = {OnUpdatePart.class}
-    )
+    @NotNull(message = "id is is required", groups = {OnUpdatePart.class})
     private Long id;
 
-    @NotBlank(message = "name is required")
+    @NotBlank(message = "name is required", groups = {OnCreatePart.class, OnUpdatePart.class})
     @Size(min = 3, max = 255,
             message = "name must must be of 2 - 255 characters",
             groups = {OnCreatePart.class, OnUpdatePart.class}
     )
     private String name;
 
-    @NotBlank(message = "description is required")
+    @NotBlank(message = "description is required", groups = {OnCreatePart.class, OnUpdatePart.class})
     @Size(min = 3,
             message = "description must contain at least 3 characters",
             groups = {OnCreatePart.class, OnUpdatePart.class}
     )
     private String description;
 
-    @NotBlank(message = "serial is required")
+    @NotBlank(message = "serial is required", groups = {OnCreatePart.class, OnUpdatePart.class})
     @Size(min = 3, max = 255,
             message = "serial must must be of 2 - 255 characters",
             groups = {OnCreatePart.class, OnUpdatePart.class}
     )
     private String serial;
 
-    @NotBlank(message = "producer is required")
+    @NotBlank(message = "producer is required", groups = {OnCreatePart.class, OnUpdatePart.class})
     @Size(max = 255,
             message = "producer must contain a maximun of 3 characters",
             groups = {OnCreatePart.class, OnUpdatePart.class}
     )
     private String producer;
 
-    @NotBlank(message = "carModel is required")
+    @NotBlank(message = "carModel is required", groups = {OnCreatePart.class, OnUpdatePart.class})
     @Size(max = 255,
             message = "carModel must contain a maximun of 3 characters",
             groups = {OnCreatePart.class, OnUpdatePart.class}
@@ -56,5 +54,5 @@ public class PartDto {
     @NotNull(message = "carId is required",
             groups = {OnCreatePart.class, OnUpdatePart.class}
     )
-    private int carId;
+    private Long carId;
 }
